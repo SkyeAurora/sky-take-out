7488d3ad5e9f394028ed860a95a9a41df9404e44
Huaperion <m15291144991@163.com>
2024-03-13 21:08:14 +0800

    新增 套餐分页查询功能

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,16 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -23,11 +22,27 @@ public class SetmealController {
 
     @PostMapping
     @ApiOperation("新增套餐")
-    public Result addSetmeal(@RequestBody SetmealDTO setmealDTO){
-        log.info("新增套餐:{}",setmealDTO);
+    public Result addSetmeal(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐:{}", setmealDTO);
 
         setmealService.addSetmeal(setmealDTO);
 
         return Result.success();
+    }
+
+    /**
+     * 分页查询套餐数据
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询套餐数据")
+    public Result<PageResult> pageSelectSetmeal(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("分页查询套餐数据,{}", setmealPageQueryDTO);
+
+        PageResult pageResult = setmealService.pageSelectSetmeal(setmealPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,12 +1,23 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
     /**
      * 新增套餐
+     *
      * @param setmealDTO
      */
     void addSetmeal(SetmealDTO setmealDTO);
+
+    /**
+     * 分页查询套餐数据
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageSelectSetmeal(SetmealPageQueryDTO setmealPageQueryDTO);
 }
