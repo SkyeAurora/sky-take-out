5f76e8065bf882abbc209a79431006a9e1b65029
Huaperion <m15291144991@163.com>
2024-03-13 12:06:31 +0800

    新增 添加菜品业务功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,10 @@
 package com.sky.mapper;
 
+import com.sky.annoation.AutoFill;
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,4 +18,11 @@ public interface DishMapper {
      */
     @Select("select count(*) from dish where category_id = #{categoryId}")
     public Integer countByCategoryId(Long categoryId);
+
+    /**
+     * 新增菜品
+     * @param dish
+     */
+    @AutoFill(OperationType.INSERT)
+    void insert(Dish dish);
 }
