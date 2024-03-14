a8d9d0a7e27586f6f7bd93b1c9b417034a0bb323
Huaperion <m15291144991@163.com>
2024-03-14 21:17:06 +0800

    新增 微信小程序端用户登录功能

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+@Mapper
+public interface UserMapper {
+    /**
+     * 根据openid查询用户是否存在
+     *
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid=#{openid}")
+    User selectByOpenId(String openid);
+
+    /**
+     * 新增微信用户
+     *
+     * @param user
+     */
+    void insert(User user);
+}
