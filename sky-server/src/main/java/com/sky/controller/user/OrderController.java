42ebc8c0f97016357e751f25658ded0bcc90cf58
Huaperion <m15291144991@163.com>
2024-03-17 10:41:43 +0800

    新增 用户支付订单功能 ps:作为个人无法正常完成支付功能，使用重定向完成支付功能

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,17 +1,20 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -22,6 +25,12 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    /**
+     * 用户下单
+     *
+     * @param ordersSubmitDTO
+     * @return
+     */
     @PostMapping("/submit")
     @ApiOperation("用户下单")
     public Result<OrderSubmitVO> submitOrders(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
@@ -29,7 +38,39 @@ public class OrderController {
 
         OrderSubmitVO orderSubmitVO = orderService.submitOrders(ordersSubmitDTO);
 
-        return Result.success();
+        return Result.success(orderSubmitVO);
+    }
+
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("生成预支付交易单：{}", orderPaymentVO);
+        return Result.success(orderPaymentVO);
+    }
+
+
+    /**
+     * 分页查询历史订单信息
+     *
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    @ApiOperation("分页查询历史订单信息")
+    public Result<PageResult> getAllOrders(OrdersPageQueryDTO ordersPageQueryDTO) {
+        log.info("分页查询历史订单信息,{}", ordersPageQueryDTO);
+
+        PageResult pageResult = orderService.getAllOrders(ordersPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 
 }
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,14 +1,42 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 
 public interface OrderService {
 
     /**
      * 用户下单
+     *
      * @param ordersSubmitDTO
      * @return
      */
     OrderSubmitVO submitOrders(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
+
+    /**
+     * 分页查询历史订单信息
+     *
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult getAllOrders(OrdersPageQueryDTO ordersPageQueryDTO);
 }
